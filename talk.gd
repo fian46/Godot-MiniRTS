db46cc0de0030d35bed6473c1a3ac7affede1225
fian46 <fian.manafe46@gmail.com>
2019-10-31 05:50:42 +0900

    Card arc

--- talk.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-#this video will talk about card creation
-#every card extending Agent.tscn
-#pathing is using flowfield
-# lets create some card
-# more complex state is needed for example attacking other card
